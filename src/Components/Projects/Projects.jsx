4c5ebbf0b8e34540d43f7bedaca0aee7b00f9387
KAMESH <kameshkamesh3876@gmail.com>
2025-03-28 16:13:51 +0530

    project added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,28 +1,28 @@
 import "./Projects.css";
-import { FaGithub } from 'react-icons/fa';
-import project1Image from "../../assets/project3.jpg";
+import { MdLiveTv } from "react-icons/md";
+import project1Image from "../../assets/ud.png";
 import project2Image from "../../assets/project2.jpg";
 import project3Image from "../../assets/project3.jpg";
 
 const Projects = () => {
   const projects = [
     {
-      title: "Smart Parking System",
-      description: "Developed an IoT-based Smart Parking System to improve urban parking efficiency using real-time data. Integrated sensors to monitor parking space availability and provide live updates via an LCD display.",
+      title: "Event Mangement website - Udhayam",
+      description: "Developed the Udhayam Website in collaboration using React to manage event registrations, provide real-time updates, and streamline participant engagement for an intercollege cultural fest. Implemented a user-friendly interface with seamless navigation, ensuring efficient event management and accessibility.",
       image: project1Image,
-      github: "https://github.com/kameshp1815/Smart-ParkingSystem.git"
+      github: "https://udhayam-csbs.vercel.app/"
     },
     {
       title: "Rock-Paper-Scissors",
       description: "Developed an interactive Rock-Paper-Scissors game using HTML, CSS, and JavaScript, featuring a user-friendly interface and responsive design for an engaging experience.",
       image: project2Image,
-      github: "https://github.com/kameshp1815/rock-paper-scissor-game.git"
+      github: "https://kamesh-rockpaper-scissor-game.netlify.app/"
     },
     {
       title: "Agriculture Machinery Rental",
       description: "Developed a dynamic Agriculture Machinery Rental System using React for the frontend. The system features a Machines page where users can browse available agricultural machinery and book rentals based on the number of days.",
       image: project3Image,
-      github: "https://github.com/kameshp1815/Agriculture-Machines-Rental.git"
+      github: "https://agriculture-machines-rental.onrender.com/"
     }
   ];
 
@@ -39,7 +39,7 @@ const Projects = () => {
               <h3>{project.title}</h3>
               <p>{project.description}</p>
               <a href={project.github} className="githubLink" target="_blank" rel="noopener noreferrer">
-                <FaGithub className="githubIcon" /> View on GitHub
+                <MdLiveTv className="githubIcon" /> View Project
               </a>
             </div>
           </div>
